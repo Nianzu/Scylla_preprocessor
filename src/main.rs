7e3ec7016afac04f2e47928f7631fc5a8250cfb8
Nico <nazucca7@gmail.com>
2025-02-13 13:31:31 -0700

    Added indexing of all algebraic notation moves

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use sacrifice::prelude::*;
+use rschess::Board;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::{env, string};
@@ -9,6 +9,7 @@ struct game {
     white_elo: Option<u16>,
     black_elo: Option<u16>,
     pgn: String,
+    moves: Vec<String>,
 }
 
 impl game {
@@ -17,6 +18,7 @@ impl game {
             white_elo: None,
             black_elo: None,
             pgn: "".to_owned(),
+            moves: vec![],
         }
     }
 
@@ -35,10 +37,40 @@ impl game {
             && self.white_elo >= Some(MIN_ELO)
             && self.black_elo >= Some(MIN_ELO)
     }
+
+    fn remove_brackets(input: String) -> String {
+        let mut bracket_depth = 0;
+        let mut output:String = "".to_owned();
+        for char in input.chars() {
+            if char == '{' {
+                bracket_depth += 1;
+            }
+            if bracket_depth == 0{
+                output.push(char);
+            }
+            if char == '}' {
+                bracket_depth -= 1;
+            } 
+        }
+        output
+    }
+
+    fn parse_moves(mut self) -> Self {
+        let beginning = game::remove_brackets(self.pgn.clone());
+        let mut intermediate: Vec<&str> = beginning.split(" ").collect();
+        intermediate.retain(|value| !value.contains(".") && !value.is_empty());
+        let end: Vec<String> = intermediate
+            .into_iter()
+            .map(|value| value.to_owned())
+            .collect();
+        self.moves = end.split_last().unwrap().1.to_vec();
+        self
+    }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
+    let board = Board::default();
 
     if args.len() < 2 {
         println!("Please include a path to the pgn you want to use");
@@ -69,12 +101,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 _ => {}
             }
         } else {
-            current_game.pgn = current_game.pgn + " " + &line.to_owned();
+            current_game.pgn = current_game.pgn + &line.to_owned();
         }
         if current_line_type == "moves" && last_line_type == "tags" {
             if current_game.is_desired() {
+                current_game = current_game.parse_moves();
                 games.push(current_game);
-                
+
                 // Debug code
                 if games.len() > 1 {
                     break;
@@ -87,5 +120,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
     println!("Games: {}", games.len());
     println!("{}", games[0].pgn);
+    println!("{:?}", games[0].moves);
+    // println!("{board}");
     Ok(())
 }
